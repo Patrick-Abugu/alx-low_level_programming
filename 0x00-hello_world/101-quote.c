6d9ff540077e541ca3979bd6d6f1444ab02ab02d
Patrick Abugu <patrickabugu@gmail.com>
2022-09-09 03:40:09 -0700

    r

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+  * main - main function
+  *
+  * Return: must return 1
+  */
+int main(void)
+{
+	write(2, "and the piece of code is useful\"-Dora Korpar, 2015-10-19\n", 59);
+	return (1)
+}
