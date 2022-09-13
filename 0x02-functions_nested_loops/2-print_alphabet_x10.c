27b34c9d96a5a5afa3f0236cafe0d3525cba6bb3
Patrick Abugu <patrickabugu@gmail.com>
2022-09-13 07:57:00 -0700

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  * print_alphabet_x10 - print x10
+  *
+  * Return: always zero
+  */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		char ch = 'a';
+
+		while (ch <= 'z')
+			_putchar(ch);
+			ch++;
+		i++;
+	}
+	_putchar('\n');
+}
