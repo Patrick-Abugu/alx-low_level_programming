e3db333a6645423d45bc6a5f589218c6ea87ee98
Patrick Abugu <patrickabugu@gmail.com>
2022-09-13 07:22:56 -0700

    all alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,16 +5,11 @@
  * Return: Always 0.
  */
 
-int main(void)
-
+void print_alphabet(void)
 {
-	void print_alphabet(void)
-	{
-		int i;
+	int i;
 
-		for (i = 'a'; i <= 'z'; i++)
-		putchar('i');
-		putchar('\n');
-		return (0);
-	}
+	for (i = 'a'; i <= 'z'; i++)
+	putchar('i');
+	putchar('\n');
 }
