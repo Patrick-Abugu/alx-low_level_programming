d83b1a9a32a4740988f352b8f7d79e3e0493e95b
Patrick Abugu <patrickabugu@gmail.com>
2022-09-13 20:28:52 -0700

    last digit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-  * _isalpha
+  * _isalpha - check if an alphabet
   *
   * @c: int to be checked
   *
@@ -9,12 +9,12 @@
 
 int _isalpha(int c)
 {
-	if (( c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 }
