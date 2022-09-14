5d2c17d00a186622a6898bc3460c704209a8ab2f
Patrick Abugu <patrickabugu@gmail.com>
2022-09-14 06:01:14 -0700

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,12 +4,12 @@
   * add - add two numbers
   *
   * @a: first number
-  * 
-  *@ b: second number
+  *
+  * @b: second number
   *
   * Return: their sum
   */
 int add(int a, int b)
 {
-	return (a + );
+	return (a + b);
 }
