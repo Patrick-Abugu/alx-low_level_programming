a3ee56c3daf2b372ad1d6d206a91a387df16ac21
Patrick Abugu <patrickabugu@gmail.com>
2022-09-14 05:03:47 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * times_table - prints 9x table
+  *
+  * Return
+  */
+
+void times_table(void)
+{
+	int i = 0;
+	int j;
+
+	while (i <= 9)
+	{
+		j = 0;
+		while (j <= 9)
+		{
+			_putchar('0' + i * j);
+			_putchar(',');
+			_putchar(' ');
+			_putchar();
+			j++;
+		}
+		i++;
+	}
+}
