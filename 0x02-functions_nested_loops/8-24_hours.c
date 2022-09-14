1d878d053e58379ecd643ee6a30daa62cef36a45
Patrick Abugu <patrickabugu@gmail.com>
2022-09-14 04:35:27 -0700

    all hour and minute

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * jack_bauer - prints all hours ans minutes
+  *
+  * Return: always return 0
+  */
+
+void jack_bauer(void)
+{
+	int hour = 0;
+	int minute;
+
+	while (hour < 25)
+	{
+		minute = 0;
+		while (minute <= 60)
+		{
+			_putchar('0' + (hour / 10));
+			_putchar('0' + (hour % 10));
+			_putchar('0' + (minute / 10));
+			_putchar('0' + (minute % 10));
+		}
+		_putchar('\n');
+	}
+}
