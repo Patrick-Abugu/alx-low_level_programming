6b7ee607f86c3ec0b48ab176a8089c1076e7f8ac
Patrick Abugu <patrickabugu@gmail.com>
2022-09-15 08:47:38 -0700

    is super

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,26 @@
+include "main.h"
+
+
+/**
+ * _isupper - checks for uppercase char
+ *
+ * @c: input argument
+ *
+ * Return: 1 if uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+
+{
+
+	while (c >= 'A' && c <= 'Z')
+
+	{
+
+		return (1);
+
+	}
+
+	return (0);
+
+}
