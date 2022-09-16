500e248fbd95295077bed04b0e12f1950825ef2a
Patrick Abugu <patrickabugu@gmail.com>
2022-09-15 21:55:42 -0700

    lorem ipsun

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle
+ *
+ * @size: no of times characte should be printed
+ *
+ */
+
+void print_triangle(int size)
+{
+	int rows;
+	int cols;
+	int hash;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+
+		rows = 1;
+		while (rows <= size)
+		{
+			for (cols = 1; cols <= (size - rows); cols++)
+			{
+				_putchar(' ');
+			}
+			for (hash = 1; hash <= rows; hash++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+			rows++;
+		}
+	}
+}
