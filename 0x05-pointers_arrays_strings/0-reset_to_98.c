6f61ad6335f2a52e8cde01c20020e0f603a633b3
Patrick Abugu <patrickabugu@gmail.com>
2022-09-19 09:14:03 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - Reset the value of n to 98
+  *
+  * @n: int to reset
+  *
+  * Return: return 0
+  */
+
+void reset_to_98(int *n)
+{
+	int p;
+
+	p = *n;
+	p = 98;
+}
