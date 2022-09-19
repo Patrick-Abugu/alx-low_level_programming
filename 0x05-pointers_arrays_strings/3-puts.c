757542ec5d5cbf6f8e59f82deb9fa346a25e6df8
Patrick Abugu <patrickabugu@gmail.com>
2022-09-19 11:13:20 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * _puts - print a char
+  *
+  * @str: string to print
+*/
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
