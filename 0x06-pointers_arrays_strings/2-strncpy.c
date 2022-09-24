31046461f8b4f25b679ce7990779d760ed0514b4
Patrick Abugu <patrickabugu@gmail.com>
2022-09-23 20:47:46 -0700

    corrections

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,28 +1,26 @@
 #include "main.h"
 
 /**
-  * strncat - concatenates two strings
+  * _strncat - concatenates two strings
   *
   * @dest: destination text
   * @src: source text
   * @n: number of times to iterate
-
   * Return: return the destination text
   */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i;
-	
+
 	i = 0;
 	while (src[i] != '\0')
 	{
 		if (i < n)
 		{
 			dest[i] = src[i];
-			dest[i] = '\0'
+			dest[i] = '\0';
 			i++;
 		}
-		
 	}
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
@@ -7,4 +8,4 @@ char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
-#endif
+#endif /*MAIN_H*/
