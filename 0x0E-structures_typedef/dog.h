d230a9385bf27d935eb94faa78ee637ae797873f
Patrick Abugu <patrickabugu@gmail.com>
2022-10-10 13:05:55 -0700

    typedef for struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,3 +16,9 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+/**
+  * dog_t - this will be used as a synonymn for struct dog
+  *
+  */
+typedef struct dog dog_t;
