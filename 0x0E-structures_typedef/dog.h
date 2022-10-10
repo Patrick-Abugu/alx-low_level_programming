2e05af6ea3b66ae394830b1826bfc70e84d537ec
Patrick Abugu <patrickabugu@gmail.com>
2022-10-10 11:22:16 -0700

    create dog  struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+  * struct dog - my dog structure
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: owner of dog
+  *
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
