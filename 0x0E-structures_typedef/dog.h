6638ded14405368f0316a15495b474053c5d7fef
Patrick Abugu <patrickabugu@gmail.com>
2022-10-10 11:53:52 -0700

    initialiaze dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,9 +7,11 @@
   * Description: some stuffs
   */
 
-struct Dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
