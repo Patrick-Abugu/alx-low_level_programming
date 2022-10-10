2b13776c3008aab3e523bf7752605d763abe8aa4
Patrick Abugu <patrickabugu@gmail.com>
2022-10-10 13:24:15 -0700

    create new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+  * *new_dog - function creates a mew dog
+  * @name: name of dog
+  * @agwe: age of dog
+  * @owner: owners name
+  *
+  */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *mydog;
+	mydog = malloc(sizeof(dog_t));
+	if (mydog == NULL)
+	{
+		return (NULL);
+	}
+
+	mydog->name = name;
+	mydog->age = age;
+	mydog->owner = owner;
+	return (mydog);
+}
+
